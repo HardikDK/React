77d4d1e283bed05066034abd97ae1b7d7d4e84a6
== <=>
2023-12-08 11:09:04 +0530

    Login Page half with half left side Header and same ase Footer done other half in progress

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import bgimage from './img/bgimg.jpeg';
+import Login from './components/Login.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <div className="bg-cover bg-center flex justify-center">
+        <img src={bgimage} className="App-logo absolute bg-black backgroundImg h-full w-full" alt="logo" />
+          <header>
+            <div className=" flex justify-left">
+              <a href="#">
+                <h6 className="text-xl text-white">Login Page</h6>
+              </a>
+            </div>
+          {/*
+            <div className=" justify-right">
+              <a href="#">
+                <h6 className="text-xl text-white">Dashboard</h6>
+              </a>
+              <a href="#">
+                <h6 className="text-xl text-white">Register</h6>
+              </a>
+              <a href="#">
+                <h6 className="text-xl text-white">Login</h6>
+              </a>
+            </div>
+          */}
+          </header>
+        <Login/>
+      </div>
+      <div className="flex justify-left">
+        <a href="#">
+          <h6 className="text-xl text-white mr-2">HOME</h6>
         </a>
-      </header>
+        <a href="#">
+          <h6 className="text-xl text-white mr-2">COMPANY</h6>
+        </a>
+        <a href="#">
+          <h6 className="text-xl text-white mr-2">PORTFOLIO</h6>
+        </a>
+        <a href="#">
+          <h6 className="text-xl text-white mr-2">BLOG</h6>
+        </a>
+      </div>
     </div>
   );
 }
